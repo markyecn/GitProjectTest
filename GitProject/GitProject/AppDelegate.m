e0e0ab9591bcedf145fd775dce549dffb3b5d283
markyecn <fly_live@sina.com>
2019-05-06 21:47:05 +0800

    add log message

--- GitProject/GitProject/AppDelegate.m
+++ GitProject/GitProject/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"log message here");
     return YES;
 }
 
