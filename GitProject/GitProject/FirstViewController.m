ec173822f1e8daf6ac2c11fd368e4706794723f4
markyecn <fly_live@sina.com>
2019-05-27 22:23:54 +0800

    列表滚动-测试提交

--- GitProject/GitProject/FirstViewController.m
+++ GitProject/GitProject/FirstViewController.m
@@ -8,7 +8,11 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+@interface FirstViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property(nonatomic,strong) UITableView *tableView;
+
+@property(nonatomic,strong) NSMutableArray *dataArray;
 
 @end
 
@@ -18,7 +22,45 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"dev change here");
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0,[UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) style:UITableViewStylePlain];
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+    [self.view addSubview:_tableView];
+    _dataArray = [NSMutableArray arrayWithArray:@[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"2",@"3",@"1",@"2",@"3",@"1",@"2",@"3",@"1",@"2",@"3",@"1",@"2",@"3",@"1",@"2",@"3"]];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:16 inSection:0];
+        CGRect rect = [self.tableView rectForRowAtIndexPath:indexPath];
+//        CGRect rect1 = [self.tableView convertRect:rect toView:self.view];
+        UIView *view =  [[UIView alloc] initWithFrame:CGRectMake(0, rect.origin.y, 200, 30)];
+        view.backgroundColor = [UIColor redColor];
+        [self.tableView addSubview:view];
+        [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionMiddle animated:YES];
+        NSLog(@"--YYYYY------%f",rect.origin.y);
+//        [self.tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:NO];
+    });
+    [_tableView reloadData];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _dataArray.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *cellId = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    static index = 0;
+    index ++;
+    NSLog(@"-----%d",index);
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 80;
+}
 
 @end
